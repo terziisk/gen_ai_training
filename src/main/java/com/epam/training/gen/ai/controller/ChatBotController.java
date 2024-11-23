fbeda543d136edd9af739bf2c00aaaed35c6ebfc
Artem Terziiskyi <terziisk@gmail.com>
2024-11-24 00:10:51 +0200

    cleanup

--- src/main/java/com/epam/training/gen/ai/controller/ChatBotController.java
+++ src/main/java/com/epam/training/gen/ai/controller/ChatBotController.java
@@ -1,11 +1,7 @@
 package com.epam.training.gen.ai.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.epam.training.gen.ai.model.ResponseModel;
-import com.epam.training.gen.ai.service.AiConnectorService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ChatBotController {
 
-  private final AiConnectorService service;
 
-  @GetMapping("/chat-response")
-  public ResponseModel getChatResponse(@RequestParam(name = "prompt") final String userPrompt) {
-    return service.getChatResponse(userPrompt);
   }
 }
